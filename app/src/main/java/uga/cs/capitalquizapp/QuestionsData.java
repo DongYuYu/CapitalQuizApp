9058b3a58ed73040d4f44b21070b3350e3e3ce93
Dong Yu Yu <scratch19@gmail.com>
2018-10-27 13:32:53 +0800

    recycle view

--- app/src/main/java/uga/cs/capitalquizapp/QuestionsData.java
+++ app/src/main/java/uga/cs/capitalquizapp/QuestionsData.java
@@ -24,6 +24,17 @@ public class QuestionsData {
             QuestionsSqlOpenHelper.QUESTIONS_COLUMN_CITY2
     };
 
+
+
+
+    private static final String[] alls = {
+            QuestionsSqlOpenHelper.QUIZ_ID,
+            QuestionsSqlOpenHelper.QUIZ_DATE,
+            QuestionsSqlOpenHelper.QUIZ_RESULT
+
+    };
+
+
     public QuestionsData( Context context ) {
         this.questionsSqlOpenHelper = QuestionsSqlOpenHelper.getInstance( context );
     }
@@ -47,6 +58,48 @@ public class QuestionsData {
     // Retrieve all job leads as a List.
     // This is how we restore persistent objects stored as rows in the job leads table in the database.
     // For each retrieved row, we create a new JobLead (Java object) instance and add it to the list.
+
+    public List<Quiz> retrieveAllQuizzes() {
+        ArrayList<Quiz> quizzes = new ArrayList<>();
+        Cursor cursor = null;
+
+        try {
+            // Execute the select query and get the Cursor to iterate over the retrieved rows
+            cursor = db.query( QuestionsSqlOpenHelper.TABLE_QUIZ, alls,
+                    null, null, null, null, null );
+            // collect all job leads into a List
+            if( cursor.getCount() > 0 ) {
+                while( cursor.moveToNext() ) {
+                    long id = cursor.getLong( cursor.getColumnIndex( QuestionsSqlOpenHelper.QUIZ_ID ) );
+                    String date = cursor.getString( cursor.getColumnIndex( QuestionsSqlOpenHelper.QUIZ_DATE ) );
+                    int result = cursor.getInt( cursor.getColumnIndex( QuestionsSqlOpenHelper.QUIZ_RESULT ) );
+
+
+                    Quiz quiz = new Quiz( date, result );
+                    quiz.setId( id );
+                    quizzes.add( quiz );
+                    Log.d( DEBUG_TAG, "Retrieved JobLead: " + quiz );
+                }
+            }
+            Log.d( DEBUG_TAG, "Number of records from DB: " + cursor.getCount() );
+        }
+        catch( Exception e ){
+            Log.d( DEBUG_TAG, "Exception caught: " + e );
+        }
+        finally{
+            // we should close the cursor
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return quizzes;
+    }
+
+
+
+
+
+
 
     public List<Question> retrieveAllJobLeads(Set<Integer> ids) {
         ArrayList<Question> questions = new ArrayList<>();
